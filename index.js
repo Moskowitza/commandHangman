e57807bf6878f3e60dc9633248cffe0e3aa2d430
moskowitza <moskowitzaaron@mac.com>
2018-04-17 09:43:48 -0400

    structuring

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+
+// * Randomly selects a word and uses the `Word` constructor to store it
+
+// * Prompts the user for each guess and keeps track of the user's remaining guessesa
+//  `Word.js` *should only* require `Letter.js`
+var Letter = require("./letter.js");
+
+//word array stores possible selections
+var wordsArr =["tofurky","tofu","kale"];
+
+//random word picker
+var wordChooser=function(){
+  var i=wordArr.length;//how many choices (3)
+  var j= Math.floor((Math.random()*i)) //return a number between 0 and wordArr.length-1
+  console.log(wordArr[j]);//console log the selected word
+}
+//run word choice
+wordChooser();
---
+++ letter.js
@@ -0,0 +1,29 @@
+
+// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+
+//require NOTHING
+
+function Letter(char) {
+    // * A string value to store the underlying character for the letter
+    this.char = char;
+    // * A boolean value that stores whether that letter has been guessed yet
+    this.guessed = false;//default to false?
+    // * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    this.state = function () {
+        if (this.guessed) {
+            this.char;  //update the displayed character
+            console.log("Correct") //show message
+            console.log(wordState)//word status with blanks filled in
+        } else {
+            console.log("Oops") //show message
+            console.log(wordState)//word status with blanks filled in
+        }
+    };
+    // * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    this.valid = function () {
+        if (response === this.char) {
+            this.guessed = true;
+        }
+    }
+};

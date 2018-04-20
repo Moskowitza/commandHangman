d8a659f6f9d265eb1a76a435466a9bcc90a1f48a
moskowitza <moskowitzaaron@mac.com>
2018-04-20 16:36:19 -0400

    getting results

--- letter.js
+++ letter.js
@@ -14,18 +14,19 @@ function Letter(char) {
             // console.log("Correct") //show message
             // console.log(this.state)//word status with blanks filled in
         } else {
-            return "_";
+            return  "_";
             // console.log("Oops") //show message
             // console.log("_")//word status with blanks filled in
         }
     };
     // * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
     this.isValid = function (guess) {
-        console.log("hi your guess made it to here with your guess of: "+guess)
-        // if (guess === this.char) {
-        //     this.guessed = true;
-            
-        // }
+        test=JSON.parse(guess);
+        if (test === this.char) {
+            this.guessed = true;
+            console.log("we have a match")
+            console.log(this.guessed)
+        }
     }
 };
 module.exports = Letter;
--- word.js
+++ word.js
@@ -4,13 +4,13 @@ var Letter = require("./letter.js");
 
 function Word(gameWord) {
     this.gameWord = gameWord;
-    console.log("in Wordjs this gameWord value is " + this.gameWord + " and the type is " + typeof gameWord);
+    // console.log("in Wordjs this gameWord value is " + this.gameWord + " and the type is " + typeof gameWord);
     this.letterArr = [];
     // * A function that returns a string representing the word. 
     this.addLetter = function (char) {
-        console.log("from wordjs line 22 gameWord: " + gameWord + " and the type is " + typeof gameWord)
+        console.log("from wordjs line 11 gameWord: " + gameWord + " and the type is " + typeof gameWord)
         var gameWordArr = gameWord.split("");
-        console.log("from wordjs line 23 gamewordArr: " + gameWordArr + " and the type is " + typeof gameWordArr)
+        console.log("from wordjs line 13 gamewordArr: " + gameWordArr + " and the type is " + typeof gameWordArr)
         for (i = 0; i < gameWordArr.length; i++) {
             var char = gameWordArr[i]
             console.log("char from add Lettermethod " + char)
@@ -26,19 +26,21 @@ function Word(gameWord) {
         //for (each this.letter.char)
         for (i = 0; i < this.letterArr.length; i++) {
             var replyString = this.letterArr[i].state();
-            console.log("this replyString value is: " + replyString + "and the type is " + typeof replyString);
+            console.log("THIS replyString value is: " + replyString + "and the type is " + typeof replyString);
             this.replyArr.push(replyString);
         }
         console.log("reply array joined: " + this.replyArr.join(""))
     };
-    this.wordString();
+    
     // * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
     this.validator = function (guess) {
         for (i = 0; i < this.letterArr.length; i++) {
             console.log("finding out if "+guess+" is a letter in our word.")
             console.log(this.letterArr);
-            this.letterArr[i].Letter.isValid(guess);
+            this.letterArr[i].isValid(guess);
+
         }
+        this.wordString();
     }
 }
 // Word()
